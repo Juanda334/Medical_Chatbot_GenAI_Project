bdaa37c281c82cb0137e16d1cf7b175f2a93625c
Juan David Valderrama Artunduaga <jd.valderrama@uniandes.edu.co>
2025-11-18 19:15:50 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical_chatbot',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is to build an AI medical chatbot using RAG approach and architecture.',
+    author='Juan David Valderrama Artunduaga',
+    license='MIT',
+    install_requires=[]
+)
